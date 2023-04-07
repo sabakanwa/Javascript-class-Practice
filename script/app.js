a2b0e276d01493cff8ee4336756fbc0ba48be7ba
saba parveen <sabakanwal5566@gmail.com>
2023-04-08 00:40:16 +0500

    Js file add

---
+++ script/app.js
@@ -0,0 +1,14 @@
+// 3-April-2023 Class practice
+alert("Thanks for your input!");
+
+//variable
+var name = "Mark";
+alert(name);
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+alert(newNum);
+
+var originalNum = 90;
+ originalNum = originalNum + 10;
+ document.write(originalNum);
